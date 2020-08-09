242bf17af4821de85fce1c5e0d28647d9b2f4a10
anuj0810 <anujagrawal@outlook.in>
2020-08-10 03:07:00 +0530

    Updates

---
+++ precache-manifest.44f8822999cdaf84a2301230d37e152a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166eee2bd4b96321ca101ddb85cc5995",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "006370ef510598e1fb04",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92a029fe76f84a062f2b",
+    "url": "/react-gh-pages/static/js/2.0a0cb9c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.0a0cb9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006370ef510598e1fb04",
+    "url": "/react-gh-pages/static/js/main.4df31676.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  }
+]);
